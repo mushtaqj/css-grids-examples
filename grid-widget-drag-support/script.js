bbbfa066b4bb9317ef7b7c9d2302d7a4b0393ab3
Mushtaq J <mushtaq.jameel@cambio.lk>
2019-09-11 20:10:17 +0530

    Added new version of grid drag and drop support and grid-widget-drag-support

---
+++ grid-widget-drag-support/script.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    var dragSrcEl = null;
+
+    //set the opacity to 40% when the target is moved
+    function handleDragStart(e) {
+        this.style.opacity = '0.4';  // this / e.target is the source node.
+
+        dragSrcEl = this;
+
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/html', this.innerHTML);
+    }
+
+    function handleDragOver(e) {
+        if (e.preventDefault) {
+            e.preventDefault(); // Necessary. Allows us to drop.
+        }
+
+        e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+        return false;
+    }
+
+    function handleDragEnter(e) {
+        // this / e.target is the current hover target.
+        this.classList.add('over');
+    }
+
+    function handleDragLeave(e) {
+        this.classList.remove('over');  // this / e.target is previous target element.
+    }
+
+    function handleDrop(e) {
+        // this / e.target is current target element.
+
+        if (e.stopPropagation) {
+            e.stopPropagation(); // stops the browser from redirecting.
+        }
+
+        // Don't do anything if dropping the same column we're dragging.
+        if (dragSrcEl != null) {
+            // Set the source column's HTML to the HTML of the column we dropped on.
+            dragSrcEl.innerHTML = this.innerHTML;
+            dragSrcEl.style.opacity = '1';
+            this.style.opacity = '1';
+            this.innerHTML = e.dataTransfer.getData('text/html');
+        }
+
+        return false;
+    }
+
+    function handleDragEnd(e) {
+        [].forEach.call(cols, function (col) {
+            col.classList.remove('over');
+        });
+    }
+
+    let cols = document.querySelectorAll('.wrapper div.draggable');
+
+    [].forEach.call(cols, function (col) {
+        col.addEventListener('dragstart', handleDragStart, false);
+        col.addEventListener('dragenter', handleDragEnter, false);
+        col.addEventListener('dragover', handleDragOver, false);
+        col.addEventListener('dragleave', handleDragLeave, false);
+        col.addEventListener('drop', handleDrop, false);
+        col.addEventListener('dragend', handleDragEnd, false);
+    });
+
+
+});
+
+function moveMe(e) {
+  console.log("Move Move",e)
+}
